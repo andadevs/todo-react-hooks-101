356e198916496c8d9d3949e27f5ae041e5f3bea5
Carlos Lenon <code@carloslenon.com>
2020-08-26 14:42:05 -0600

    add antd lib dependencies add carousel implementation add dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+/**
+ * @name MyApp
+ * @file _app.js
+ * @class Base Next App file
+ * @classdesc Main app.
+ * @since v1.0.0
+ * @author boykland/clenondavis <clenondavis@outlook.com>
+ */
+
+//#region styles
+import "../styles/_general.scss";
+import "antd/dist/antd.css";
+//#endregion
+
+/**
+ * @name MyApp
+ * @memberof MyApp
+ * @description MyApp component
+ * @param {Component} Component - Component rendered
+ * @param {Object} pageProps - Component props
+ * @return {PROMISE/MODEL} - A promise with The city model just created
+ */
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,50 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+/**
+ * @name Pages
+ * @file index.js
+ * @class Base File
+ * @classdesc Main app.
+ * @since v1.0.0
+ * @author boykland/clenondavis <clenondavis@outlook.com>
+ */
 
-export default function Home() {
+//#region dependencies
+import { Carousel } from "antd";
+//#endregion
+
+/**
+ * @function
+ * @name Main
+ * @memberof Pages
+ * @type {COMPONENT}
+ * @description Main component
+ * @return {COMPONENTS} - A components context
+ */
+export default function Main() {
+  const contentStyle = {
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+  };
+
+  function onChange(a, b, c) {
+    console.log(a, b, c);
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    <Carousel afterChange={onChange}>
+      <div>
+        <h3 style={contentStyle}>1</h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>2</h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>3</h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>4</h3>
+      </div>
+    </Carousel>
+  );
 }
